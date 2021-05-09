5b0bee95102f2bb3c341043787db105b1c06c7ae
Hasan-star-creat <74924051+Hasan-star-creat@users.noreply.github.com>
2021-05-09 18:21:37 +0600

    sub total and tax with total price added

--- app1.js
+++ app1.js
@@ -16,7 +16,8 @@ function ticketSelect(id, isTrue){
       }   
   }
   updateTicketPrice(id, quantity, singlePrice);
-   
+  updateSubTotal();
+  updateTotal(); 
 }
 
 function getQuantity(id){
@@ -35,6 +36,28 @@ function getQuantity(id){
 
     function updateTicketPrice(id , qty , tk){
     const money  = document.getElementById(`${id}-total`);
-     money.innerText = (qty * parseFloat(tk)).toFixed(2);
+    if(qty < 1){
+        money.innerText = 00;
+    }else{
+        money.innerText = (qty * parseFloat(tk)).toFixed(2);
+    }
      
     }
+
+    // sub total 
+   function  updateSubTotal(){
+      const first = document.getElementById('first-class-total').innerText;
+      const second = document.getElementById('second-class-total').innerText;
+     const subTotal = document.getElementById('sub-total');
+     subTotal.innerText = parseFloat(first) + parseFloat(second);       
+   }
+
+    // update Total value with tax
+   function updateTotal(){
+     const subTotal = document.getElementById('sub-total').innerText;
+     const tax = document.getElementById('tax').innerText = parseFloat(subTotal) * 0.1;
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = (tax + parseFloat(subTotal)).toFixed(2);
+
+   }
+     
